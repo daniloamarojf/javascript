fa0155fcbff226e05d3c8b150fa6ce02e74c9d00
daniloamarojf <daniloramarojf@gmail.com>
2025-06-03 21:44:52 -0300

    Atividade_03

---
+++ Atividade_03/js/script.js
@@ -0,0 +1,16 @@
+
+
+const produtos = [
+    { nome: 'Sabonete Natural', valor: 15.00, categoria: 'cosmeticos' },
+    { nome: 'Sampoo Orgânico', valor: 25.00, categoria: 'consmeticos'},
+    { nome: 'Granola Artesanal', valor: 18.50, categoria: 'alimentos' },
+    { nome: 'Mel Puro', valor: 22.00, categoria: 'aliemntos'},
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    const bntTodos = document.getElementById('resultado');
+    const bntCosmeticos = document.getElementById('resultado');
+    const bntAliemntos = document.getElementById('resultado');
+
+
+} );
