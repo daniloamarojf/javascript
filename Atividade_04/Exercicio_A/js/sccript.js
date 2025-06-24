885d4e82465ae5e3dad230ce3ceaa1f4d867d764
daniloamarojf <daniloramarojf@gmail.com>
2025-06-24 14:52:10 -0300

    Exercicio A

---
+++ Atividade_04/Exercicio_A/js/sccript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('verificar');
+    const resultado = document.getElementById('resultado');
+
+    
+});
