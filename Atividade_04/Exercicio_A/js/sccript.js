1db292f611522d6a61232831b9fdff9c4163e479
daniloamarojf <daniloramarojf@gmail.com>
2025-06-24 21:42:31 -0300

    Exercicio A funcionando (ajustar CSS)

--- Atividade_04/Exercicio_A/js/sccript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const botao = document.getElementById('verificar');
-    const resultado = document.getElementById('resultado');
-
-    
-});
---
+++ Atividade_04/Exercicio_A/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botao = document.getElementById('verificar');
+    const resultado = document.getElementById('resultado');
+    const input = document.getElementById('numero');
+
+    
+
+    function ParImpar() {
+
+        resultado.innerHTML = "";
+
+        const numero = parseInt(input.value);
+
+
+        if (isNaN(numero)) {
+            resultado.innerHTML = "Digite um número inteiro";
+            return;
+        }
+            
+
+        if (numero % 2 === 0) {
+            resultado.innerHTML = "Número é PAR" 
+        } else {
+            resultado.innerHTML = "Número é ÍMPAR"
+        };
+
+    };
+
+    botao.addEventListener('click', ParImpar);
+
+    
+});
