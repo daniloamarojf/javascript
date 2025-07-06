92c0a619b6da3113811ec2331b64320fd41eeb1c
daniloamarojf <daniloramarojf@gmail.com>
2025-07-05 22:45:32 -0300

    Par e Impar funcionando

--- Atividade _04_Refatorada/src/modulos/parImpar.js
+++ Atividade _04_Refatorada/src/modulos/parImpar.js
@@ -2,13 +2,37 @@ import { ValidarEntrada } from '../utilitarios/utilitarios.js';
 
 export function CalcularParImpar() {
 
-    //const numero = ValidarEntrada();
-    const numero = document.getElementById('numero');
+    const numero = ValidarEntrada();
     const resultado = document.getElementById('resultado');
+
+    // Mesmo com a validação foi necessário confirmar se o
+    // número é null, pois a validação ´so valida e retorna 
+    // o valor null.
+    if (numero === null) {
+        return;
+    }
 
     if (numero % 2 === 0) {
         resultado.innerHTML = "Número é PAR"
     } else {
         resultado.innerHTML = "Número ÍMPAR"
     }
-};
+}
+
+
+export function CalcularMaiorMenor() {
+
+    const numero = ValidarEntrada();
+    const resultado = document.getElementById('resultado');
+    
+    if (numero1 === numero2 && numero2 === numero3) {
+        resultado.innerHTML = "Os números são iguais";
+    } else {
+        const maior = Math.max(numero1, numero2, numero3);
+        const menor = Math.min(numero1, numero2, numero3);
+
+        resultado.innerHTML = `O MAIOR número é: ${maior}  |  O MENOR número é ${menor}`
+    }
+}
+
+
--- Atividade _04_Refatorada/src/utilitarios/utilitarios.js
+++ Atividade _04_Refatorada/src/utilitarios/utilitarios.js
@@ -1,12 +1,34 @@
+// Aqui estou usando export para permitir 
+// que a função seja usada em outros arquivos
 export function ValidarEntrada () {
+    // Criei duas constantes uma para pegar o número 
+    // que o usuario digitou, refere-se ao id=numero no html
+    // A outra para ler o lugar onde a mensagem será 
+    // exibida, refere-se ao id=resultado no html
     const numero = document.getElementById('numero');
     const resultado = document.getElementById('resultado');
+     
+    // Eu estava primeiro convertendo o numero em Number
+    // para depois verificar se está vazio, com isso 
+    // a validação não funcionava (o Number transforma
+    // o valor vazio em 0)
+    const texto = numero.value.trim();
 
-    if (isNaN(numero)) {
+    if (texto === '') {
         resultado.innerHTML = "Por favor, preencha o campo corretamente."
         return null;
     }
 
+    // A constante abaixo pega o valor de input e converte em número
+    const numeroInput = Number(texto);
+
+    // isNan: Verifica se não é um numero valido e se é vazio
+    if (isNaN(numeroInput)) {
+        resultado.innerHTML = "Por favor, preencha o campo corretamente."
+        return null;
+    }
+
+    return numeroInput; // Se passar pelos paramentros retorna o valor
 
 }
 
