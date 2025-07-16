10dad0e7667dcc541e4f3d88c4d49e87c5848bb2
daniloamarojf <daniloramarojf@gmail.com>
2025-07-16 16:44:25 -0300

    Validações para notas

--- Atividade _04_Refatorada/src/utilitarios/utilitarios.js
+++ Atividade _04_Refatorada/src/utilitarios/utilitarios.js
@@ -40,3 +40,61 @@ export function AdicionarEventoBotao (idBotao, funcao) {
     // chamando a função 
     document.getElementById(idBotao).addEventListener('click', funcao);
 }
+
+
+
+/*
+export function validarNota (nota1Bim, nota2Bim, nota3Bim, nota4Bim) {
+    
+    // Campos vazios
+    if (nota1Bim === '' || nota2Bim === '' || nota3Bim === '' || nota4Bim === '') {
+        alert("Todos os campos devem ser preenchidos.")
+        return null;
+    }
+
+    // Converter em número
+    [nota1Bim, nota2Bim, nota3Bim, nota4Bim] = 
+    [nota1Bim, nota2Bim, nota3Bim, nota4Bim].map(Number);
+
+    // Números válidos
+    if (isNaN(nota1Bim) || isNaN(nota2Bim) || isNaN(nota3Bim) || isNaN(nota4Bim)) {
+        alert("Por favor, preencha o campo corretamente.")
+        return null;
+    }
+
+    // Número negativo
+    if (nota1Bim < 0 || nota2Bim < 0 || nota3Bim < 0 || nota4Bim < 0) {
+        alert("Nota do aluno não pode ser negativa.")
+        return null;
+    }
+
+    // Número maior que 25
+    if (nota1Bim > 25 || nota2Bim > 25 || nota3Bim > 25 || nota4Bim > 25) {
+        alert("Nota do aluno não pode ser maior que 25 pontos.")
+        return null;
+
+    }
+
+    return {nota1Bim, nota2Bim, nota3Bim, nota4Bim};
+}
+    
+
+export function validarNome(nome) {
+
+    // Incluir validação de numero e caracteres especiais
+
+    // Dividindo em partes, sem espaço no icicio e final
+    const nomePartes = nome.trim().split(/\s+/);
+
+    // Se há no mínino duas palavras
+    // Cada com dus letras
+    const nomeValido = nomePartes.length >=2 && nomePartes.every(parte => parte.length >= 2);
+    
+    // Mensagem se nome inválido
+    if (!nomeValido) {
+        alert('Digite o nome completo (nome e sobrenome).')
+        return null;
+    }
+    return nome;
+}
+    */
